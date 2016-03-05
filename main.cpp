446cb9e075fac6b0749613b955edf929325d473e
Sergey Vidyuk <sir.vestnik@gmail.com>
2016-03-05 20:16:33 +0600

    serialize

--- main.cpp
+++ main.cpp
@@ -1,63 +1,138 @@
 #include <iostream>
 #include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
 
+// Generic metaprogramming tools
+template<typename... T>
+struct make_void {typedef void type;};
+
+template<typename... T>
+using require = typename make_void<T...>::type;
+
+// Members reflection simulation tools
 template<typename C, typename T>
-using member_ptr = T C::*;
+using MemberPtr = T C::*;
 
-template<typename C, typename T, member_ptr<C, T> M>
-struct member_info {
+template<typename C, typename T, MemberPtr<C, T> M>
+struct MemberInfo {
     static const char* name;
+    static const T& getValue(const C& c) {return (c.*M);}
     using type = T;
     using class_type = C;
+};
+template<typename T>
+struct TypeInfo;
+
+template<typename T>
+using Members = typename TypeInfo<T>::members;
+
+//     Reflectable concept (requires members to be enlisted)
+template<typename T, typename = require<>>
+struct IsReflectable: public std::false_type {};
+
+template<typename T>
+struct IsReflectable<T, require<Members<T>>>: public std::true_type {};
+
+template<typename T>
+using Reflectable = typename std::enable_if<IsReflectable<T>::value, T>::type;
+
+// Writing tools
+
+//     Direct writable concept (requres operator<<(std::ostream&, const T&))
+template<typename T, typename = require<>>
+struct IsDirectWritable: public std::false_type {};
+
+template<typename T>
+struct IsDirectWritable<T, require<decltype(std::declval<std::ostream&>() << std::declval<const T&>())>>: public std::true_type {};
+
+template<typename T>
+using DirectWritable = typename std::enable_if<IsDirectWritable<T>::value, T>::type;
+
+//     Reflec-writable concept (requires Reflectable but not DirectWritable)
+template<typename T>
+using ReflectWritable = typename std::enable_if<IsReflectable<T>::value && !IsDirectWritable<T>::value, T>::type;
+
+//     Writer class
+template<typename T>
+class Writer final {
+public:
+    static void write(std::ostream& out, const T& t) {
+        static_assert(
+            IsReflectable<T>::value || IsDirectWritable<T>::value,
+            "Writer can't write value of non-reflectable type without operator<<(std::ostream&, const T&) overload."
+        );
+        Writer writer(out);
+        writer.write<T>(t);
+    }
+
+private:
+    Writer(std::ostream& out): mOut(out) {}
+
+    template<typename U>
+    void write(const DirectWritable<U>& val) {
+        mOut << val;
+    }
+
+    template<typename U>
+    void write(const ReflectWritable<U>& val) {
+        mOut << '{' << std::endl;
+        write<U>(val, std::make_index_sequence<std::tuple_size<Members<U>>::value>());
+        mOut << '}';
+    }
+
+    void swallow(std::initializer_list<int>) {}
+
+    template<typename U, size_t... I>
+    void write(const Reflectable<U>& val, std::index_sequence<I...>) {
+        swallow({ (writeKV(std::tuple_element<I, Members<U>>::type::name, std::tuple_element<I, Members<U>>::type::getValue(val)), 0)... });
+    }
+
+    template<typename U>
+    void writeKV(const char* key, const U& val) {
+        mOut << key << ": ";
+        write<U>(val);
+        mOut << std::endl;
+    }
+
+private:
+    std::ostream& mOut;
+};
+
+// User types
+
+struct GeoPos {
+    double lon;
+    double lat;
+};
+template<>const char* MemberInfo<GeoPos, double, &GeoPos::lon>::name = "lon";
+template<>const char* MemberInfo<GeoPos, double, &GeoPos::lat>::name = "lat";
+template<>
+struct TypeInfo<GeoPos>{
+    using members = std::tuple<
+        MemberInfo<GeoPos, double, &GeoPos::lon>,
+        MemberInfo<GeoPos, double, &GeoPos::lat>
+    >;
 };
 
 struct User {
     std::string name;
-    int age;
-
-    int get_age() const {return age;}
-    int& get_age() {return age;}
-    void set_age(int val) {age = val;}
-
-    template<typename Str>
-    void set_name(Str&& str) {name = std::forward<Str>(str);}
+    GeoPos location;
 };
+template<>const char* MemberInfo<User, std::string, &User::name>::name = "name";
+template<>const char* MemberInfo<User, GeoPos, &User::location>::name = "location";
 template<>
-const char* member_info<User, std::string, &User::name>::name = "name";
-template<>
-const char* member_info<User, int, &User::age>::name = "age";
-template<>
-const char* member_info<User, int() const, &User::get_age>::name = "get_age";
-template<>
-const char* member_info<User, int&(), &User::get_age>::name = "get_age";
-template<>
-const char* member_info<User, void(int), &User::set_age>::name = "set_age";
-
-// Can't have pointer to template member but can have pointer to instantion of template member
-template<>
-const char* member_info<User, void(const std::string&), &User::set_name>::name = "set_name";
-template<>
-const char* member_info<User, void(std::string&&), &User::set_name>::name = "set_name";
-
-template<typename T>
-struct Point {
-    T x;
-    T y;
+struct TypeInfo<User>{
+    using members = std::tuple<
+        MemberInfo<User, std::string, &User::name>,
+        MemberInfo<User, GeoPos, &User::location>
+    >;
 };
-
-using PointF = Point<double>;
-template<>
-const char* member_info<PointF, double, &PointF::x>::name = "x";
-template<>
-const char* member_info<PointF, double, &PointF::y>::name = "y";
 
 int main() {
-    std::cout << member_info<User, std::string, &User::name>::name << std::endl;
-    std::cout << member_info<User, int() const, &User::get_age>::name << std::endl;
-    std::cout << member_info<User, int&(), &User::get_age>::name << std::endl;
-    std::cout << member_info<User, void(int), &User::set_age>::name << std::endl;
-    std::cout << member_info<User, void(const std::string&), &User::set_name>::name << std::endl;
-    std::cout << member_info<User, void(std::string&&), &User::set_name>::name << std::endl;
-    std::cout << member_info<PointF, double, &PointF::x>::name << std::endl;
+    User vasya = {"vasya", GeoPos{54.0, 85.0}};
+    Writer<User>::write(std::cout, vasya);
+    std::cout << std::endl;
     return 0;
 }
