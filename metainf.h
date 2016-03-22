055bb3513f5a7c9499b2f9850feed16b6dfca222
Sergey Vidyuk <sir.vestnik@gmail.com>
2016-03-22 13:06:38 +0600

    Simplify metainf search function

--- metainf.h
+++ metainf.h
@@ -28,21 +28,24 @@ template<typename MetaTag, class C>
 struct has_metainf<MetaTag, C, require<MembersMetainf<MetaTag, C>>>: public std::true_type {};
 
 namespace detail {
-template<typename MetaTag, typename M, typename MembersTuple, size_t Idx, typename = void>
-struct metainf_getter {
-static const typename MetaTag::type* get(Member<M> m) {
-    if (m == std::tuple_element<Idx, MembersTuple>::type::member)
-        return &std::tuple_element<Idx, MembersTuple>::type::value;
-    return metainf_getter<MetaTag, M, MembersTuple, Idx + 1>::get(m);
-}
-};
 // recursion termination
 template<typename MetaTag, typename M, typename MembersTuple, size_t Idx>
-struct metainf_getter<MetaTag, M, MembersTuple, Idx, typename std::enable_if<Idx >= std::tuple_size<MembersTuple>::value>::type> {
-static const typename MetaTag::type* get(Member<M> m) {
+typename std::enable_if<
+    Idx >= std::tuple_size<MembersTuple>::value,
+    const typename MetaTag::type*
+>::type get_metainf(Member<M> m) {
     return nullptr;
 }
-};
+
+template<typename MetaTag, typename M, typename MembersTuple, size_t Idx>
+typename std::enable_if<
+    Idx < std::tuple_size<MembersTuple>::value,
+    const typename MetaTag::type*
+>::type get_metainf(Member<M> m) {
+    if (m == std::tuple_element<Idx, MembersTuple>::type::member)
+        return &std::tuple_element<Idx, MembersTuple>::type::value;
+    return get_metainf<MetaTag, M, MembersTuple, Idx + 1>(m);
+}
 }
 
 template<typename MetaTag, typename M>
@@ -51,5 +54,5 @@ const typename MetaTag::type* get_metainf(M m) {
         is_member<M>::value &&
         has_metainf<MetaTag, typename member_trait<M>::class_type>::value,
         "M must be a pointer to member of some class with metainf corresponding to MetaTag attached");
-    return detail::metainf_getter<MetaTag, M, MembersMetainf<MetaTag, typename member_trait<M>::class_type>, 0>::get(m);
+    return detail::get_metainf<MetaTag, M, MembersMetainf<MetaTag, typename member_trait<M>::class_type>, 0>(m);
 }
